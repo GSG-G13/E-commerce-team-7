ff48381e85a33e71aa3aaa5b3cbcfe4cee9d36dc
momenmarwan <momenalgaga82@gmail.com>
2023-05-21 20:55:55 +0300

    trager auto save

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import serverError from "./middleware/serverError.js";
+import clientError from "./middleware/client.js";
+
 
 dotenv.config();
 
@@ -11,9 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 
-
-app.get("/", (req, res) => {
-  res.json({ result: "Hello" });
-});
+app.use(clientError);
+app.use(serverError);
 
 export default app;
