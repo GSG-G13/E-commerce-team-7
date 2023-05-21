b92340a50d5c249e01694698abc0a91ddbdb69d3
alaaahmade <alaaahmade66@gmail.com>
2023-05-21 22:04:53 +0300

    requested changes done

--- server/src/controllers/cart/deleteProduct.js
+++ server/src/controllers/cart/deleteProduct.js
@@ -1,15 +1,13 @@
 import { deleteFromCart } from '../../database/query/index.js';
 
 export const deleteProduct = (req, res) => {
-  console.log('hi');
-  // const user_id = res.userData.id;
   const user_id = 1
   const { product_id } = req.params
 
   deleteFromCart({user_id, product_id})
-    // .then((data) => data.rows[0])
     .then((data) => {
       if(data.rowCount){
+        console.log(data.rows);
       res.status(201).json({
         error:false,
         message: 'successfully'
--- server/src/database/query/cart/deleteFromCart.js
+++ server/src/database/query/cart/deleteFromCart.js
@@ -2,7 +2,7 @@ import connection from '../../config/connection.js'
 
 export const deleteFromCart = ({user_id, product_id}) => {
   const sql = {
-    text : "DELETE FROM cart WHERE user_id=$1 AND product_id=$2 RETURNING 'SUCCESSFULLY'",
+    text : "DELETE FROM cart WHERE user_id=$1 AND product_id=$2",
     values: [user_id, product_id]
   }
   return connection.query(sql)
