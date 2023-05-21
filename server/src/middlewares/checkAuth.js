c4ab5cff3c4a22f181e72ec79c3fe0beea3174b2
alaaahmade <alaaahmade66@gmail.com>
2023-05-21 15:22:31 +0300

    Create checkAuth middlewares & Promise verify, sign Relates #8

---
+++ server/src/middlewares/checkAuth.js
@@ -0,0 +1,21 @@
+import { verify } from '../utils/promisesSign';
+
+export const checkAuth = (req, res, next) => {
+  const { token } = req.cookies;
+  
+  if(!token) {
+    return res.json({
+      message: 'UnAuthorized',
+    })
+  }
+  verify(token)
+    .then((decoded) => {
+      res.userData = decoded
+      next()
+    }).catch(() =>{
+      res.clearCookie('token').json({
+        message: 'UnAuthorized',
+      })
+    } )
+
+}
