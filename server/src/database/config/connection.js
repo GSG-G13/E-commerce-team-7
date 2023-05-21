5421cd1df2017258c7fbad17595a38f8acacaf1b
momenmarwan <momenalgaga82@gmail.com>
2023-05-21 14:39:06 +0300

    set database connection

---
+++ server/src/database/config/connection.js
@@ -0,0 +1,8 @@
+import pg from 'pg';
+import {} from 'dotenv/config';
+
+const connectionString = process.env.DB_URL
+
+const connection = new pg.Pool({connectionString})
+
+export default connection;
