3466393279e61aa9128de1d74628414c23901c93
alaaahmade <alaaahmade66@gmail.com>
2023-05-22 19:59:01 +0300

    fix eslint errors

--- server/src/middleware/isLogged.js
+++ server/src/middleware/isLogged.js
@@ -2,15 +2,18 @@ import { verify } from '../utils/index.js';
 
 export const isLogged = (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) next();
-  verify(token)
-    .then(() => {
-      res.json({
-        status: 401,
-        msg: 'this user is logged',
+  if (!token) {
+    next();
+  } else {
+    verify(token)
+      .then(() => {
+        res.json({
+          status: 401,
+          msg: 'this user is logged',
+        });
+      }).catch(() => {
+        res.clearCookie('token');
+        next();
       });
-    }).catch(() => {
-      res.clearCookie('token');
-      next();
-    });
+  }
 };
