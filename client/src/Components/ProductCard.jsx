9b49ad5435daf419c89158066cace81c30824cf7
momenmarwan <momenalgaga82@gmail.com>
2023-05-24 12:51:40 +0300

    any thing

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import '../assets/styles/cards.css';
 import { Link } from 'react-router-dom';
 
-function ProductCard() {
+function ProductCard({product : {name,image , description , price, discount, details}}) {
   return (
     <div className="product-card">
       <div className="product-tumb">
-        <img src="https://w7.pngwing.com/pngs/323/773/png-transparent-sneakers-basketball-shoe-sportswear-nike-shoe-outdoor-shoe-running-sneakers-thumbnail.png" width="100%" alt="" />
+        <img src={image} width="100%" alt="" />
 
       </div>
       <div className="product-details">
-        <span className="product-catagory">Shoes, boot</span>
-        <h4><a href="#link">Men leather boot</a></h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+        <span className="product-catagory">{details}</span>
+        <h4><a href="#link">{description}</a></h4>
+        <p>{details}</p>
         <div className="product-bottom-details">
-          <div className="product-price">$230.99</div>
+          <div className="product-price">{price}</div>
           <div className="product-links">
             <Link href="#link"><i className="fa fa-shopping-cart" /></Link>
           </div>
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,16 +1,28 @@
 /* eslint-disable import/prefer-default-export */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/styles/layout.css';
 import ProductCard from '../Components/ProductCard';
 
 export function HomePage() {
   const [category, setCategory] = useState('All');
   const [price, setPrice] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api//products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
   const handleCategory = (e) => {
     setCategory(e.target.value);
   };
-  console.log(price);
   const filterData = (arrayToFilter, value) => arrayToFilter.filter((product) => product === value);
+  useEffect(() => {
+    const newArray = filterData(products, category);
+    setProducts(newArray);
+  }, [category, price]);
 
   return (
     <div className="small-container">
@@ -27,7 +39,7 @@ export function HomePage() {
         </select>
       </div>
       <div className="product-list">
-        <ProductCard />
+        {products.map((product) => <ProductCard product={product} key={product.id} />)}
       </div>
       <div className="page-btn" id="pagination">
         <span>1</span>
