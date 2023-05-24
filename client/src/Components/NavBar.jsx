b5be37c1a02178e64f64677da552f29880824dad
alaaahmade <alaaahmade66@gmail.com>
2023-05-24 13:39:29 +0300

    Add create checkAuth on navBar

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,18 +1,30 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useState } from 'react';
 import '../assets/styles/layout.css';
 import { Link, useNavigate } from 'react-router-dom';
 
 export function NavBar() {
   const navigate = useNavigate();
+  const [Auth, setAuth] = useState(false);
 
   const logoutHandler = () => {
-    fetch(`/api/user/logout`)
+    fetch('/api/user/logout')
       .then((data) => data.json())
       .then(({ status }) => {
         if (status === 200) {
-          navigate('/')
+          navigate('/');
         }
       });
   };
+
+  useEffect(() => {
+    const token = document.cookie;
+    if (token.startsWith('token')) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  });
 
   return (
     <div>
@@ -24,9 +36,17 @@ export function NavBar() {
           <nav>
             <ul id="MenuItems">
               <li><Link to="/">Home</Link></li>
-              <li><Link to="signup">SignUp</Link></li>
-              <li><Link to="signin">SignIn</Link></li>
-              <li><i className="fa-solid fa-arrow-right-from-bracket" onClick={logoutHandler} /></li>
+              {Auth
+                // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+                ? <li><i className="fa-solid fa-arrow-right-from-bracket" onClick={logoutHandler} /></li>
+                : (
+                  <>
+                    <li><Link to="signup">SignUp</Link></li>
+                    <li><Link to="signin">SignIn</Link></li>
+                  </>
+                )}
+
+              {/* <li><i className="fa-solid fa-arrow-right-from-bracket" onClick={logoutHandler} /></li> */}
             </ul>
           </nav>
           <Link to="cart"><i className="fa fa-shopping-cart" /></Link>
