0f208a93cf628f6456bc59a7ad9f5cd0f2b3c49d
alaaahmade <alaaahmade66@gmail.com>
2023-05-27 22:48:39 +0300

    refactoring & add loader

--- client/src/Pages/CartPage.jsx
+++ client/src/Pages/CartPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable array-callback-return */
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
